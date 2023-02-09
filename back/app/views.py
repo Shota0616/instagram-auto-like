5e74af005039fe43866648a504eb348a058db371
ShotaIsoda <ju.oh0616@gmail.com>
2023-02-09 23:58:53 +0900

    20230209

--- back/app/views.py
+++ back/app/views.py
@@ -6,8 +6,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
 from app.forms import InstaAutoForm
@@ -17,12 +19,9 @@ logger = logging.getLogger(__name__)
 
 # app = Celery('tasks', backend='amqp', broker='amqp://admin:admin@127.0.0.1:5672')
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
 class IndexView(TemplateView):
     template_name = "app/index.html"
+
 
 class AutoLikeView(FormView):
     template_name = 'app/app.html'
@@ -30,6 +29,7 @@ class AutoLikeView(FormView):
 
     def form_valid(self, form):
         return render(self.request, 'app/app.html', {'form': form})
+
 
 class AutoLikeConfirmView(FormView):
     template_name = 'app/confirm.html'
@@ -42,13 +42,54 @@ class AutoLikeConfirmView(FormView):
         auto_like(form)
         return render(self.request, 'app/confirm.html', {'form': form})
 
-
-    # 自動実行関数
-    def auto_like(self, form):
-        # formから取得した値を変数に格納
+    """
+    以下インスタグラムいいね自動実行の関数
+    """
+    def main(self, form):
+        # 変数
         email = form.cleaned_data['insta_email']
         password = form.cleaned_data['insta_password']
         num_of_times = form.cleaned_data['insta_num_of_times']
+        # 他の変数（後でformから入力可能にするかも）
+        tags = ['バイク', 'ドラッグスター']
 
-        # 処理
-        driver.get('https://google.com')
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        wait = WebDriverWait(driver=driver, timeout=30)
+
+        # ログイン実行
+        insta_login(email, password, driver)
+        # タグ検索用urlセット（リストで取得）
+        tagurllist = tagsearch(tags, driver)
+
+    """
+    ログイン
+    """
+    def insta_login(self, email, password, driver):
+        # 変数
+        submit_btn = '_acan'
+
+        # urlを開く
+        driver.get(https://www.instagram.com/)
+        # 要素がすべて抽出できるまで待機
+        wait.until(EC.presence_of_all_elements_located)
+        # メールアドレスとパスワードを入力，及びログイン
+        driver.find_element_by_name('username').send_keys(username)
+        time.sleep(1)
+        driver.find_element_by_name('password').send_keys(password)
+        time.sleep(1)
+        driver.find_element_by_class_name(submit_btn)
+        wait.until(EC.presence_of_all_elements_located)
+
+    def tagsearch(self, tags, driver)
+        # 変数
+        tagsearchurl = 'https://www.instagram.com/explore/tags/'
+        tagurllist = []
+        # tagが複数ある場合は分割
+        for tag in tags:
+            tagurllist.append(tagsearchurl + tag)
+
+        return tagurllist
+
+    def like()
