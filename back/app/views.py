d8ea2031900b37bc5db403839b1936718e006996
ShotaIsoda <ju.oh0616@gmail.com>
2023-03-13 00:57:05 +0900

    20230313

--- back/app/views.py
+++ back/app/views.py
@@ -10,6 +10,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 import time
 import random
@@ -54,16 +56,6 @@ class MainView(View):
         return render(self.request, 'app/confirm.html', params)
 
     @staticmethod
-    def test(email, password):
-        print(email)
-        print(password)
-        driver = MainView.conection_chrome()
-        driver.get("https://github.com/Shota0616")
-        print("testtest")
-        driver.close()
-
-
-    @staticmethod
     def conection_chrome():
         # selenium
         options = webdriver.ChromeOptions()
@@ -81,47 +73,63 @@ class MainView(View):
     """
     @staticmethod
     def main(email, password):
-
+        # try:
+        logger.info(f'{email}:処理開始')
         driver = MainView.conection_chrome()
         wait = WebDriverWait(driver=driver, timeout=30)
         # 変数
+        like_btn_class = '_aamw'
         # 他の変数（後でformから入力可能にするかも）
         tags = ['バイク', 'ドラッグスター']
 
         # ログイン実行
         MainView.insta_login(email, password)
-        # タグ検索用urlセット（リストで取得）
-        tagurllist = MainView.tag_search(tags)
         # タグの数を取得
         tagurllist_len = len(tags)
         # タグ数に応じてタグ一つに対するいいね回数を選択
         onetaglike_num = 1000 / tagurllist_len
+        i = 0
         # タグごとにいいね実行
-        for tag in tagurllist:
+        for tag in tags:
+            # タグ検索用
+            MainView.tag_search(driver, tag, email)
+            time.sleep(random.randint(2, 5))
+            i = i + 1
+            logger.info(f'{email}:{i}個目のタグのいいね処理を開始します')
+            # 新しいタブでurlを開く
+            # driver.execute_script(f"window.open('{tag}');")
             driver.get(tag)
-            time.sleep(1)
-            MainView.newest_post()
+            wait.until(EC.presence_of_all_elements_located)
+            time.sleep(random.randint(5, 7))
+            # 最新の投稿まで移動
+            MainView.newest_post(driver, wait, email)
             # 最新の投稿をいいね
-            driver.find_elements_by_class_name('_aagw')[9].click()
-            time.sleep(random.randict(3, 6))
-            if MainView.already_like() == True:
+            driver.find_elements(By.CLASS_NAME,'_aagw')[9].click()
+            time.sleep(random.randint(2, 5))
+            if MainView.already_like(driver) == True:
                 pass
             else:
-                driver.find_element_by_class_name('_aamw').click()
-                time.sleep(random.randict(3, 5))
+                driver.find_element(By.CLASS_NAME,like_btn_class).click()
+                time.sleep(random.randint(3, 5))
             # 次の投稿をいいね × onetaglike_num
             like_counter = 0
             while like_counter < onetaglike_num:
-                if MainView.already_like() == True:
+                if MainView.already_like(driver) == True:
                     pass
                 else:
-                    driver.find_elements_by_xpath('//button[@class="_abl-"]')[2].click()
+                    driver.find_elements(By.XPATH,'//button[@class="_abl-"]')[1].click()
                     time.sleep(random.randint(3, 6))
-                    driver.find_element_by_class_name('_aamw').click()
-                    time.sleep(random.randict(3, 5))
+                    driver.find_element(By.CLASS_NAME,like_btn_class).click()
+                    time.sleep(random.randint(3, 5))
                     like_counter += 1
         # ブラウザを閉じる
         driver.quit()
+
+        # 例外処理（現状すべての例外をキャッチ）
+        # except:
+        #     # ブラウザを閉じる
+        #     driver.quit()
+        #     logger.error('何かしらのエラーが発生しました')
 
 
 
@@ -134,56 +142,59 @@ class MainView(View):
         driver = MainView.conection_chrome()
         wait = WebDriverWait(driver, timeout=30)
         # 変数
-        submit_btn_class = '_acan'
+        submit_btn_class = '_aj1-'
 
         # urlを開く
         driver.get('https://www.instagram.com/')
         # 要素がすべて抽出できるまで待機
         wait.until(EC.presence_of_all_elements_located)
         # メールアドレスとパスワードを入力，及びログイン
-        time.sleep(1)
+        time.sleep(random.randint(3, 5))
         driver.find_element(By.NAME,'username').send_keys(email)
-        time.sleep(1)
+        time.sleep(random.randint(2, 5))
         driver.find_element(By.NAME,'password').send_keys(password)
-        time.sleep(1)
-        driver.find_element(By.CLASS_NAME, submit_btn_class).click()
+        time.sleep(random.randint(2, 5))
+        driver.find_elements(By.CLASS_NAME, submit_btn_class)[1].click()
         wait.until(EC.presence_of_all_elements_located)
 
-        print('インスタグラムにログインしました。')
+        logger.info(f'{email}:ログインしました')
 
-    """
-    タグをurlに変換し配列に格納
-    """
-    @staticmethod
-    def tag_search(tags):
-        # 変数
-        tagsearchurl = 'https://www.instagram.com/explore/tags/'
-        tagurllist = []
-        tagurllist_len = 0
-        # tagが複数ある場合は分割
-        for tag in tags:
-            tagurllist_len += 1
-            tagurllist.append(tagsearchurl + tag)
-        print('タグを配列に格納しました。')
-        return tagurllist
-
+    # """
+    # タグをurlに変換し配列に格納
+    # """
     # @staticmethod
-    # def tagurllist_len(tags):
-    #     tagurllist_len = 0
+    # def tag_search(tags, email):
+    #     # 変数
+    #     tagsearchurl = 'https://www.instagram.com/explore/tags/'
+    #     tagurllist = []
+    #     # tagが複数ある場合は分割
     #     for tag in tags:
-    #         tagurllist_len += 1
-    #     return tagurllist_len
+    #         tagurllist.append(tagsearchurl + tag)
+    #     logger.info(f'{email}:タグを配列に格納しました')
+    #     return tagurllist
+
+    @staticmethod
+    def tag_search(driver, tag, email):
+        # 変数
+        search_btn_class = 'svg[aria-label="検索"]'
+        search_field_class = 'input[aria-label="検索語句"]'
+        time.sleep(random.randint(3, 6))
+        driver.find_element(By.CSS_SELECTOR,search_btn_class).click()
+        time.sleep(random.randint(3, 6))
+        driver.find_element(By.CSS_SELECTOR,search_field_class).send_keys('#' + tag)
+        time.sleep(random.randint(3, 6))
+        action = webdriver.ActionChains(driver)
+        action.send_keys(Keys.ENTER).perform()
+        logger.info(f'{email}:タグ検索OK')
 
     """
     既にいいねを押しているかチェック
     """
     @staticmethod
-    def already_like():
-        driver = MainView.conection_chrome()
-        wait = WebDriverWait(driver, timeout=30)
-
-        likebtnstatus = driver.get_attribute('aria-label')
-        if likebtnstatus == '「いいね！」を取り消す':
+    def already_like(driver):
+        # 変数
+        like_btn_class = 'svg[aria-label="「いいね！」を取り消す"]'
+        if len(driver.find_element(By.CSS_SELECTOR,like_btn_class)) > 0:
             return True
         else:
             return False
@@ -192,16 +203,17 @@ class MainView(View):
     最新の投稿まで移動
     """
     @staticmethod
-    def newest_post():
-        driver = MainView.conection_chrome()
+    def newest_post(driver, wait, email):
         wait = WebDriverWait(driver=driver, timeout=30)
+        time.sleep(random.randint(4, 6))
         # 変数
-        newest_post = '_aagw'
-        target = driver.find_element_by_class_name(newest_post)[10]
+        newest_post_class = '_aanc'
+        # newest_post_class = '//*[@id="mount_0_0_ia"]/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/article/h2'
+        # newest_post_class = 'div[class="_ac7v _aang"]'
+        target = driver.find_element(By.CSS_SELECTOR,newest_post_class)
+        #target = driver.find_element(By.XPATH,newest_post_class)
         actions = ActionChains(driver)
         actions.move_to_element(target)
         actions.perform()
-
-        print('最新の投稿まで移動しました。')
-
         wait.until(EC.presence_of_all_elements_located)
+        logger.info(f'{email}:最新の投稿まで移動しました')
